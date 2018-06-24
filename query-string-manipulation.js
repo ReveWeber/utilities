9b07b2f0147635e931bdde82e2598c39d3dd699f
Rebecca Weber <reveweber@gmail.com>
2018-06-24 07:48:55 -0400

    Adding query string manipulation code and test page

---
+++ query-string-manipulation.js
@@ -0,0 +1,51 @@
+/* query-string-manipulation: 
+Functions to read parameters out of the query string 
+and update the browser URL when parameters change, 
+keeping default values out of the query string and 
+coexisting peacefully with additional parameters. */
+
+var default1 = "default1";
+var param1;
+
+/* parseQuery: 
+look for parameters in the URL query string
+if not found, assign default values */
+function parseQuery() {
+    var queryString = window.location.search.substr(1);
+    // set all parameters to their default values
+    param1 = default1;
+    if (queryString.length > 0) {
+        // if there's a query string, check for each param within it
+        var val1 = queryString.match(/.*param1=([^&]+).*/i);
+        if (val1 && val1.length > 1) {
+            param1 = val1[1];
+        }
+    }
+}
+
+/* updateQuery:
+update parameter values in current URL
+maintain any other parameter values
+keep parameters with default values out of URL */
+function updateQuery() {
+    var newUrl = window.location.href;
+    // clean out valueless parameters to simplify ensuing matching
+    newUrl = newUrl.replace(/(.*[?&])param1(&(.*))?$/, "$1$3");
+    if (param1 !== default1) {
+        if (newUrl.match(/[?&]param1=/)) {
+            newUrl = newUrl.replace(/(.*[?&]param1=)[^&]*(.*)/, '$1' + param1 + '$2');
+        } else if (newUrl.indexOf('?') > 0) {
+            newUrl = newUrl + '&param1=' + param1;
+        } else {
+            newUrl = newUrl + '?param1=' + param1;
+        }
+    } else {
+        newUrl = newUrl.replace(/(.*[?&])param1=[^&]*&?(.*)/, '$1$2');
+    }
+
+    // tidy up
+    if (newUrl.match(/[?&]$/)) {
+        newUrl = newUrl.slice(0, -1);
+    }    
+    window.history.pushState('', '', newUrl);
+}
--- readme.md
+++ readme.md
@@ -5,13 +5,15 @@ This repo is for boilerplate code and helper functions that aren't big enough to
 ### Subfolders
 
 Each has its own readme.
-* ga-boilerplate: Google Analytics tracking for internal promotions and product lists
-* high-scores: ajax-accessed database-stored high score list
-* settings: Terminal and Visual Studio Code settings-type files
+* **ga-boilerplate**: Google Analytics tracking for internal promotions and product lists
+* **high-scores**: ajax-accessed database-stored high score list
+* **settings**: Terminal and Visual Studio Code settings-type files
 
 ### Top-Level Files
 
-* fix-special-characters.js and special-character-cleaning.png <br />
+* **fix-special-characters.js** and **special-character-cleaning.png** <br />
 "Flatten" certain non-ASCII characters to their ASCII equivalent and strip the rest.
-* liquid-yaml-cheatsheet.md <br />
+* **liquid-yaml-cheatsheet.md** <br />
 Syntax and examples for the parts of Liquid and YAML I've found most useful in building Jekyll layouts.
+* **query-string-manipulation.js** and **query-string-tester.html** <br />
+Read parameters out of the query string and update the browser URL when parameters change, keeping default values out of the query string and coexisting peacefully with additional parameters. To use the test page you'll have to edit the page path appropriately.
